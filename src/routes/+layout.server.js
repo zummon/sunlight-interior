e5a715d6238dc92a1db65214a63a0a0ac1258002
zummon <zummon.space@gmail.com>
2023-04-08 21:18:29 +0700

    1 file modified

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,6 @@
 export const load = async ({}) => {
-	const model = await import(`../lib/model.json`).then((xp) => xp.default);
-	let services = await import(`../lib/services.json`).then((xp) => xp.default);
+	const model = await import("../lib/model.json").then((xp) => xp.default);
+	let services = await import("../lib/services.json").then((xp) => xp.default);
 
 	return {
 		services: services.map((service) => ({ ...model, ...service })),
