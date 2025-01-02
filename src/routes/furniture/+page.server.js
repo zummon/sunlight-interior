e25a6e75157d86c65e2309d424969f73f749a563
zummon <zummon.space@gmail.com>
2025-01-02 21:55:20 +0700

    save

---
+++ src/routes/furniture/+page.server.js
@@ -0,0 +1,8 @@
+import furnitures from '../../lib/furniture.json'
+
+export function load ({  }) {
+
+	return {
+		furnitures,
+	}
+}
---
+++ src/routes/furniture/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { data } = $props()
+	let shape = { wide: '3 / 2', tall: '2 / 3' }
+</script>
+
+<div class="w-fit mx-auto sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6">
+	{#each data.furnitures as furniture}
+		<div class="mb-4">
+			<img class="h-60 object-cover" style="aspect-ratio: { furniture['image-shape'] || 'auto' }" src={furniture['image-source']} alt={furniture['image-detail']}>
+			{furniture['image-detail']}
+		</div>
+	{/each}
+</div>
